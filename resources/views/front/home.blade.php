24bd9f3e43deecba22d22c15fe99a78796a6e435
umanga.bhattarai <umanga.bhattarai@diagonal.software>
2025-03-06 14:15:37 +0545

    update title

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>NS Skate Park</title>
+    <title>MMS</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
 
